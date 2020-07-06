3be03d9d34df2bf88032c9e05649505b332c4e63
Ilya Kanischev <m4rtinid3n@yandex.ru>
2020-07-06 09:14:00 +0300

    промежуточное сохранение

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,73 @@
-let buttonOpen = document.querySelector('.profile__button');
-let buttonClose = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
-let inputName = popup.querySelector('.popup__input_type_name');
-let inputJob = popup.querySelector('.popup__input_type_job');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let formElement = popup.querySelector('.popup__content');
+const buttonOpenProfile = document.querySelector('.profile__button');
+const buttonCloseProfile = document.querySelector('.popup__close');
+const buttonOpenElements = document.querySelector('.profile__add-button');
+const buttonCloseElements = document.querySelector('.popup__close_elements');
+const popupProfile = document.querySelector('.popup_profile');
+const popupElement = document.querySelector('.popup_element');
+const inputName = popupProfile.querySelector('.popup__input_type_name');
+const inputJob = popupProfile.querySelector('.popup__input_type_job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const formElement = popupProfile.querySelector('.popup-profiles');
+const cardsList = document.querySelector('.elements');
+const cardsTemplate = document.querySelector('.template-elements');
+const formElements = popupElement.querySelector('.popup-elements');
+const nameElement = popupElement.querySelector('.popup__input_type_name-element');
+const linkElement = popupElement.querySelector('.popup__input_type_src-element');
 
-function popupOpen() {
-    popup.classList.add('popup_opened');
-    inputName.value = profileName.textContent;
-    inputJob.value = profileJob.textContent;
+function CloneCard(item) {
+    const card = cardsTemplate.content.cloneNode(true);
+    card.querySelector('.element__title').textContent = item.name;
+    card.querySelector('.element__image').src = item.link;
+    card.querySelector('.element__delete_button').addEventListener('click', deleteCard);
+    cardsList.prepend(card);
 }
 
-function popupClose() {
-    popup.classList.remove('popup_opened');
+function deleteCard(e) {
+    const card = e.target.closest('.element');
+    card.remove();
+}
+
+function addCard(card, container) {
+    container.prepend(card);
+  }
+
+initialCards.forEach(function (item) {
+    CloneCard(item);
+});
+  
+function popupToggle(popup) {
+    popup.classList.toggle('popup_opened');
+}
+
+function saveProfileInfo() {
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
+    popupToggle(popupProfile);
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
-    popupClose();
-}
+    popupToggle(popupProfile);
+} 
 
-buttonOpen.addEventListener('click', popupOpen);
-buttonClose.addEventListener('click', popupClose);
+function cardFormSubmitHandler(evt) {
+    evt.preventDefault();
+    const name = nameElement.value;
+    const link = linkElement.value;
+    const cardItem = {name,link,}
+    const card = CloneCard(cardItem);
+    addCard(card, cardsList)
+    togglePopup(popupElement)
+    formElements.reset()
+  }
+
+
+buttonOpenProfile.addEventListener('click', saveProfileInfo);
+buttonCloseProfile.addEventListener('click', () => popupToggle(popupProfile));
+buttonOpenElements.addEventListener('click', () => popupToggle(popupElement));
+buttonCloseElements.addEventListener('click', () => popupToggle(popupElement));
 formElement.addEventListener('submit', formSubmitHandler);
+formElements.addEventListener('submit', cardFormSubmitHandler);
